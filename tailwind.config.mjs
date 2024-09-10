976815deb9e04b1e3875dcebda767a3b6402115d
Danish <danishsidd524@gmail.com>
2024-09-10 21:13:43 +0500

    add: header section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,24 +2,15 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    screens: {
-      sm: "375px",
-      md: "768px",
-      lg: "1200px",
-    },
     fontFamily: {
       heading: "Inter, sans-serif",
       body: "Karla, sans-serif",
     },
-    container: {
-      center: true,
-      padding: {
-        sm: "1.5rem",
-        md: "2rem",
-        lg: "5.5rem",
+    extend: {
+      screens: {
+        xs: "400px",
       },
     },
-    extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
 };
